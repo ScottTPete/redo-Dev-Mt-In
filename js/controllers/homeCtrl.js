9b36f81a32d00cb52ddb18c2dc0e63eb83a4012e
Scott Peterson <stpeterson28@yahoo.com>
2016-04-15 02:31:36 -0600

    Done with project.Some weird shit going on.

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,28 +1,6 @@
 angular.module('devMtIn')
-	.controller('homeCtrl', function($scope, profileSvc) {
+	.controller('homeCtrl', function($scope, profileSvc, friendSvc) {
 		
-	
-	//ng-model="myProfile" is data binding from controller to view
-	//checks for profileId if it exists call svc function to get profile and puts it on scope
-	$scope.getProfile = function() {
-		var profileId = JSON.parse(localStorage.getItem('profileId'));
-		console.log(profileId);
-		
-		if(profileId) {
-			profileSvc.getProfile(profileId.profileId).then(function(profile) {
-				$scope.myProfile = profile.data;
-				console.log($scope.myProfile);
-			})
-			.catch(function(err) {
-				console.log(err);
-			})
-		}
-	}
-	
-	//Must be invoked to actually do anything.
-	$scope.getProfile();
-	
-	
 	
 	//sets sort options for friends list, used by ng-options
 	$scope.sortOptions = [
@@ -34,7 +12,7 @@ angular.module('devMtIn')
 			display: 'Descending',
 			value: true
 		}
-	]
+	];
 	
 	
 	//set to ng-disabled which disables it's element
@@ -42,7 +20,7 @@ angular.module('devMtIn')
 	
 	
 	//sets edit button text
-	$scope.editStatus = "Click to edit!"
+	$scope.editStatus = "Click to edit!";
 	
 	$scope.toggleEdit = function() {
 		if($scope.disableEdit== true) {
@@ -52,14 +30,39 @@ angular.module('devMtIn')
 			$scope.disableEdit = true;
 			$scope.editStatus = "Click to edit!"
 		}
-	}
+	};
+	
+	
+	
 	
 	
 	//when save is clicked run this. Pass in myProfile from scope. This brings the profile from controller to service where the service function stores it locally
 	$scope.saveProfile = function(profile) {
 		profileSvc.saveProfile(profile);
 		$scope.disableEdit = true;
-	}
+	};
+	
+	
+	//ng-model="myProfile" is data binding from controller to view
+	//checks for profileId if it exists call svc function to get profile and puts it on scope
+	$scope.getProfile = function() {
+		var profileId = JSON.parse(localStorage.getItem('profileId'));
+		//		console.log(profileId);
+
+		if(profileId) {
+			profileSvc.getProfile(profileId.profileId).then(function(profile) {
+				$scope.myProfile = profile.data;
+		      //console.log($scope.myProfile);
+				friendSvc.findFriendsFriends(profile.data);
+			})
+				.catch(function(err) {
+				console.log(err);
+			})
+		}
+	};
+
+	//Must be invoked to actually do anything.
+	$scope.getProfile();
 	
 	
 	//calls delete profile from service and clears the scope
@@ -71,7 +74,36 @@ angular.module('devMtIn')
 		.catch(function(err) {
 			console.log(err);
 		})
-	}
+	};
+	
+	$scope.findFriends = function(query) {
+		
+		if($scope.custom === true) {
+			$scope.custom = false;
+		} else {
+			$scope.custom = true;
+		}
+		
+		friendSvc.findFriends($scope.myProfile._id, query).then(function(response) {
+//			console.log(response);
+			$scope.potentialFriends = response.data;
+		})
+	};
+	
+	$scope.custom = true;
+	
+	$scope.addFriend = function(friendId) {
+		friendSvc.addFriend($scope.myProfile._id, friendId).then(function(response) {
+			$scope.getProfile();
+		})
+	};
+	
+	$scope.removeFriend = function(friendId) {
+		friendSvc.removeFriend($scope.myProfile._id, friendId).then(function(response) {
+//			console.log(response);
+			$scope.getProfile();
+		})
+	}; 
 	
 	profileSvc.serviceTest();
 	
